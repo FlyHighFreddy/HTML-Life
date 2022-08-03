5522873407b9c672bc3026fdd1adbac35ee902f0
FlyHighFreddy <fred.bittner1@gmail.com>
2022-08-03 17:01:36 -0600

    Added javascript, but it doesn't work

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log('hello world')
+
+//this button is supposed to pop up when you click "click me" button on the HTML.
+onclick = null
+let btn = document.getElementById ('butt')
+btn.onclick (0) {alert("You are headed for excellence");}
