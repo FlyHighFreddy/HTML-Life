78ea425be1d6f620b0a2fd87234116ab28980414
FlyHighFreddy <fred.bittner1@gmail.com>
2022-08-03 23:22:56 -0600

    verify Targeting Script Works

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
 console.log('hello world')
 
-//this button is supposed to pop up when you click "click me" button on the HTML.
-onclick = null
-let btn = document.getElementById ('butt')
-btn.onclick (0) {alert("You are headed for excellence");}
+(() => {
+    const message = document.querySelector('#message');
+
+    // check if the Geolocation API is supported
+    if (!navigator.geolocation) {
+        message.textContent = `Your browser doesn't support Geolocation`;
+        message.classList.add('error');
+        return;
+    }
+
+    // handle click event
+    const btn = document.querySelector('#show');
+    btn.addEventListener('click', function () {
+        // get the current position
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    });
+
+
+    // handle success case
+    function onSuccess(position) {
+        const {
+            latitude,
+            longitude
+        } = position.coords;
+
+        message.classList.add('success');
+        message.textContent = `Your location: (${latitude},${longitude})`;
+    }
+
+    // handle error case
+    function onError() {
+        message.classList.add('error');
+        message.textContent = `Failed to get your location!`;
+    }
+})();
