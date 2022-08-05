db71efcf3a76c63025593561abb6d29bb242f553
FlyHighFreddy <fred.bittner1@gmail.com>
2022-08-05 01:41:22 -0600

    Call For Fire button added

--- script.js
+++ script.js
@@ -24,7 +24,7 @@
         } = position.coords;
 
         message.classList.add('success');
-        message.textContent = `YOUR LOCATION: (${latitude},${longitude}) MISSLE INBOUND`;
+        message.textContent = `VISUAL FRIENDLY LOCATION: (${latitude},${longitude})`;
     }
 
 
@@ -34,3 +34,21 @@
         message.textContent = `TARGET SEQUENCE INTERRUPTED`;
     }
 })();
+
+function launchHellfire(){
+    let h = 0;
+    let missileQuantity = document.getElementById("5lineRequest").value;
+
+    if (isNaN(missileQuantity)){
+        alert("Invalid Call for fire request. Please select number of missiles to fire.");
+
+    }
+    
+    while (h < missileQuantity) {
+        const agm = '<img src="hellfire.gif" height="100" />'
+        h++;
+        document.getElementById("missileImpact");
+        missileImpact.insertAdjacentHTML("afterend", agm);
+
+    }
+}
